b3e34770029a208305de772654eeb0ee8faa6c4f
Senja24 <ikaagus958@gmail.com>
2024-03-19 11:49:51 +0700

    fix text working

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+      <h1>Hey, I'am alive!!</h1>
+      <h2>Visit me on <a href="https://cleek.id" target="_blank">cleek</a> </h2>
+    </div>
+  )
+}
+
+export default home
